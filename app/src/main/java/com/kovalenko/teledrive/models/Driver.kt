9ca99f0ffd83ec284427c15465459cf08baa6793
bananabeard <kostikololo@gmail.com>
2018-05-21 09:03:52 +0300

    Minor changes.

--- app/src/main/java/com/kovalenko/teledrive/models/Driver.kt
+++ app/src/main/java/com/kovalenko/teledrive/models/Driver.kt
@@ -3,6 +3,8 @@ package com.kovalenko.teledrive.models
 import java.util.*
 
 class Driver(
-        var driverName: String = "",
-        var birthDate: Date? = null
+        var firstName: String = "",
+        var secondName: String = "",
+        var birthDate: Date? = null,
+        var isWorking: Boolean = false
 )
